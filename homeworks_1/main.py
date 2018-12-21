c37986e385bf74ec281e589a4170909dd4c709d6
bublik77 <andriy.bobrovych@gmail.com>
2018-12-21 11:19:14 +0200

    first homework

---
+++ homeworks_1/main.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+"""
+This is a simple example of homework task
+which printing some information
+about my favorite song
+"""
+
+
+artist = "Димна Суміш"
+genre = "Rock"
+durationInSeconds = 328
+#it is one of my favorite songs when I have a bad  mood
+
+song = "Танцюй Танцюй"
+year=2009
+
+print(artist)
+print(song,"," "duration ",durationInSeconds,"second," "year of published", year)
